48cbde7da4a442f8b75316d13da5a3b97bd500bb
Blaben <blabensolutions@gmail.com>
2025-05-24 14:20:28 +0000

    advance finance app

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,15 @@
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expense = float(input("Enter your total monthly expenses: "))
+
+
+#converting the str values to integer values
+#float(monthly_income)
+#float(monthly_expense)
+
+monthly_savings = monthly_income - monthly_expense
+annual_savings = monthly_savings * 12
+interest = annual_savings * 0.05
+projected_savings = annual_savings + interest
+
+print(f"\nYour monthly savings are {round(monthly_savings)}")
+print(f"Projected savings after one year, with interest, is: {round(projected_savings)}")
