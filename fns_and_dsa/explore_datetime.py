23c208f774146432a1a03b9588837a6efd073463
Blaben <blabensolutions@gmail.com>
2025-06-08 22:01:16 +0000

    imported the datetime module and created a function to display current date

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time:", formatted_date)
