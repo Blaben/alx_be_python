f8667e62cc32134378e8392369349701d1188869
Blaben <blabensolutions@gmail.com>
2025-06-20 09:35:19 +0000

    working on the transactions

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+#Depositing method
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+#Withdrawal method
+    def withdraw(self, amount):
+        if amount > 0 and self.__account_balance >= amount:
+            self.__account_balance -= amount
+            return True #Withdrawal successful
+        elif amount > self.__account_balance:
+            return False #Withdrawal failed
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+
+
+
